779549c7e8409d1bf8de08a7f1776d6c47576a0d
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-09 21:35:11 -0600

    Home and contact views are now created

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
+// Controllers
+ContactController = require('./controllers/ContactController');
+HomeController = require('./controllers/HomeController');
+
 app.set('view engine', 'ejs');
 
 // The set attribute is used to change any default ejs settings when it comes to filepaths etc.
 // Remember that you have to actually include the name of the target directory, and then give it the name that you want to refrence it by
 app.set('views', path.join(__dirname + '/public/views'));
-app.use('/public', express.static('public'));
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
+// Fire Controllers
+ContactController(app)
+HomeController(app)
+
 
 app.listen(3000);
 console.log("Currently Listening To Port 3000");
